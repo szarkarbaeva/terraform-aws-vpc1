de8c1c664882ba6ecd923db4f63358582663245e
szarkarbaeva <szarkarbaeva@gmail.com>
2025-04-12 15:50:36 +0000

    first module version

---
+++ main.tf
@@ -0,0 +1,20 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "${var.environment}-vpc"
+  }
+}
+
+resource "aws_subnet" "main1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.subnet1_cidr
+
+  tags = {
+    Name = "${var.environment}-subnet1"
+  }
+}
+
+variable vpc_cidr {}
+variable subnet1_id {}
+variable environment {}
